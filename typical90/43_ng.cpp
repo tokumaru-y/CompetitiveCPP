7229f422e0060edd1d33248bbf5d38fc2757ea69
tokumaru-y <tokumaru.program.yuuki@gmail.com>
2024-08-17 13:35:31 +0900

    43 ng

---
+++ typical90/43_ng.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+vector<int> dx{ 1, 0, -1, 0 };
+vector<int> dy{ 0, 1, 0, -1 };
+
+int main()
+{
+    int H, W;cin >> H >> W;
+    int sx, sy, ex, ey;cin >> sx >> sy >> ex >> ey;
+    sx--;sy--;ex--;ey--;
+    vector<vector<char>> grid(H, vector<char>(W));
+    rep(i, H)rep(j, W) {
+        cin >> grid[i][j];
+    }
+    vector<vector<int>> dist(H, vector<int>(W, 10000000));
+    queue<vector<int>> q;
+    q.push(vector<int>{sx, sy, -1});
+    dist[sx][sy] = 0;
+    while (!q.empty()) {
+        vector<int> t = q.front();q.pop();
+        int x = t[0];int y = t[1];int pre = t[2];
+        for (int i = 0;i < 4;i++) {
+            int nx = x + dx[i];int ny = y + dy[i];
+            if (nx < 0 || nx >= H || ny < 0 || ny >= W)continue;
+            if (grid[nx][ny] == '#')continue;
+
+            int nextCnt = dist[x][y];
+            if (i != pre && pre != -1)nextCnt++;
+            if (nextCnt >= dist[nx][ny])continue;
+            dist[nx][ny] = nextCnt;
+            q.push(vector<int>{ nx, ny, i});
+        }
+    }
+
+    cout << dist[ex][ey] << endl;
+}
