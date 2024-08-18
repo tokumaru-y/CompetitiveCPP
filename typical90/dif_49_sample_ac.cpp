490436fe2481f290fa066398fcaabcc2abac542d
tokumaru-y <tokumaru.program.yuuki@gmail.com>
2024-08-18 12:32:34 +0900

    no exec 49 too difficult

---
+++ typical90/dif_49_sample_ac.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+vector<int> dx{ 1, 0, -1, 0 };
+vector<int> dy{ 0, 1, 0, -1 };
+
+int main()
+{
+}
