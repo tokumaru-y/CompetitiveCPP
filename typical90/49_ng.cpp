aa51e985353e9d1734d5ace77d13aeb7c894ab37
tokumaru-y <tokumaru.program.yuuki@gmail.com>
2024-08-24 09:53:30 +0900

    tmp commit

---
+++ typical90/49_ng.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+vector<int> dx{ 1, 0, -1, 0 };
+vector<int> dy{ 0, 1, 0, -1 };
+
+void input() {}
+
+void solve() {}
+
+void output() {}
+
+int main()
+{
+    input();
+    solve();
+    output();
+}
