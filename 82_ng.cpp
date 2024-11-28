5653533b9d6bb7bb717c28db17aa5620d2f768d2
tokumaru-y <tokumaru.program.yuuki@gmail.com>
2024-11-28 21:28:16 +0900

    tmp

---
+++ 82_ng.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+vector<int> dx{ 1, 0, -1, 0 };
+vector<int> dy{ 0, 1, 0, -1 };
+ll L,R;
+ll MOD = 1000000007;
+ll ans = 0;
+
+void input() {}
+
+void solve() {}
+
+void output() {}
+
+int main()
+{
+    input();
+    solve();
+    output();
+}
